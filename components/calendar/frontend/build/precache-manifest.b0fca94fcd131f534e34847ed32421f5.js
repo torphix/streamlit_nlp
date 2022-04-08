7fe5ba6cc556aa9f9f909477d351f24c5cc3cc6c
torphix <jessedeng97@gmail.com>
2022-04-08 10:44:27 +0100

    adjustments

---
+++ components/calendar/frontend/build/precache-manifest.b0fca94fcd131f534e34847ed32421f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031a58feb429137b4c4539630d70dade",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eef5b99274a6e6c721f3",
+    "url": "./static/js/2.fb943edc.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.fb943edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e2f2dd326f8de6a69c",
+    "url": "./static/js/main.ffe975b3.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
