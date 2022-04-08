6cfd372a5ac30be7906fefff29e7e7a6dcde02b8
torphix <jessedeng97@gmail.com>
2022-04-08 13:05:39 +0100

    basic data analyitcs

--- streamlit.py
+++ streamlit.py
@@ -1,19 +1,23 @@
 import datetime
+import numpy as np
 import pandas as pd
 import streamlit as st
 from nlp import NLPProcessor
 from datetime import datetime
 # import streamlit.components.v1 as components
-from utils import read_json, add_entry, get_sentiment_text
+from utils import read_json, add_entry, get_sentiment_text, get_emotion_scores
 
 # Globals
 nlp = NLPProcessor()
-DF = pd.DataFrame(read_json())
+DATA = read_json()
+DF = pd.DataFrame(DATA)
 
 # Methods
 def reload_dataframe():
+    global DATA
     global DF
-    DF = pd.DataFrame(read_json())
+    DATA = read_json()
+    DF = pd.DataFrame(DATA)
 
 def post_entry():
     text = st.session_state.journal_entry
@@ -33,6 +37,18 @@ def post_entry():
 #     url="http://localhost:3001",
 # )
 # calendar = calendar_component(calendarName="Calendar", dataSource=DF.to_json())
+
+# Data analysis UI
+st.header('Sentiment Overview')
+emotion_data = get_emotion_scores(DATA)
+
+emotion_df = pd.DataFrame(
+    [list(emotion_data.values())],
+    columns=list(emotion_data.keys()),
+)
+st.bar_chart(data=emotion_df.T, width=0, height=0, use_container_width=True)
+
+# Journal UI
 journal_entry = st.text_area("Today's Entry", 
                             value="",
                             height=None,
@@ -41,12 +57,17 @@ journal_entry = st.text_area("Today's Entry",
                             help=None,
                             on_change=None)
 
+
 st.button("Submit",
           key='submit_entry',
           on_click=post_entry)
 
 
 st.header('Journal Entries')
+st.markdown(
+    f"""<div style='text-align:center'>
+         😠 Anger 🤢 Disgust 😨 Fear 😃 Joy 😐 Neutral 😔 Sad 😲 Suprise
+    </div> <br>""", unsafe_allow_html=True)
 st.markdown('***')
 for i in reversed(DF.index):
     entry = DF.loc[i]
@@ -60,6 +81,7 @@ for i in reversed(DF.index):
         f"""<div style='text-align:center'>
             {get_sentiment_text(entry['sentiments'])}
         </div> <br>""", unsafe_allow_html=True)
-    st.text(entry['text'])
+    st.markdown(entry['text'])
     st.markdown("***")
         
+# Data visulization
--- utils.py
+++ utils.py
@@ -24,3 +24,34 @@ def get_sentiment_text(scores):
         out += f' {round(score["score"], 2)} '
     return out
         
+        
+def get_emotion_scores(entries):
+    anger, disgust, fear, joy, neutral, sadness, surprise = \
+        0,0,0,0,0,0,0
+    for entry in entries:
+        sentiments = entry['sentiments']
+        for s in sentiments:
+            if s['label'] == 'anger':
+                anger += s['score']
+            elif s['label'] == 'disgust':
+                disgust += s['score']
+            elif s['label'] == 'fear':
+                fear += s['score']
+            elif s['label'] == 'joy':
+                joy += s['score']
+            elif s['label'] == 'neutral':
+                neutral += s['score']
+            elif s['label'] == 'sadness':
+                sadness += s['score']
+            elif s['label'] == 'surprise':
+                surprise += s['score']
+    return {
+       'anger': round(anger, 2),
+       'disgust': round(disgust, 2),
+       'fear': round(fear, 2),
+       'joy': round(joy, 2),
+       'neutral': round(neutral, 2),
+       'sadness': round(sadness, 2),
+       'surprise': round(surprise, 2),
+    }
+        
